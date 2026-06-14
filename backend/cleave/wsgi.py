989e5fe8e4b1808e8a4aaaab17d260de2d3d16ab
Poola Binu sai <149651174+Binusai@users.noreply.github.com>
2026-06-15 03:46:27 +0530

    Update wsgi.py

--- backend/cleave/wsgi.py
+++ backend/cleave/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cleave.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cleave.settings')
 
 application = get_wsgi_application()
